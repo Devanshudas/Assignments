55e6f67bfef430d55a68685d27c0b98221bf2f42
Devanshudas <101145122+Devanshudas@users.noreply.github.com>
2023-11-02 17:50:20 +0530

    Add files via upload
    Case Study

---
+++ policymanagement/src/main/java/com/hexaware/policymanagement/services/IPolicyServicesImp.java
@@ -0,0 +1,39 @@
+package com.hexaware.policymanagement.services;
+
+import java.util.List;
+
+import com.hexaware.policymanagement.entities.Policy;
+
+public class IPolicyServicesImp implements IPolicyServices {
+
+	@Override
+	public boolean createPolicy(Policy policy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updatePolicy(Policy policy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePolicy(Policy policy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Policy getPolicyById(int policy_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Policy> getAllPolicy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
